f4c1eb7cd50bb1ac5c9d697f5d852d7b7cfc7557
Gragolha <luka.100.nerozi@gmail.com>
2021-12-14 21:43:44 -0300

    Organização do JS

---
+++ Lukas/FUNDAMENTOS/Exercicios/organização.js
@@ -0,0 +1,10 @@
+console.log("Sentença de código");
+
+{
+  {
+    console.log("Hello");
+    console.log("World!"); // Padrão do curso
+  }
+}
+
+// O Javascript é organizado em Sentenças de Códigos que podem ou não terminar  com ";" e também blocos de código "{...}"
