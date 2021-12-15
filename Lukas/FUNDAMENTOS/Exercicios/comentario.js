0a804aa6b8f332556098dca3a0fa5bc5eaa033e5
Gragolha <luka.100.nerozi@gmail.com>
2021-12-14 23:31:41 -0300

    novo

---
+++ Lukas/FUNDAMENTOS/Exercicios/comentario.js
@@ -0,0 +1,14 @@
+// comnetário de uma linha
+console.log("linha 1");
+/*
+ comentário de
+ múltiplas linhas
+ */
+console.log("linha 2");
+/*
+ * comentário de
+ * múltiplas linhas
+ */
+console.log("linha 3");
+
+// Evitar comentários inúteis ou coisas óbvias.
