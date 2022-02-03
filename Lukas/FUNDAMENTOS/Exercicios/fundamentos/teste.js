2281bfed1eb602066f484193fd84709ef4e08e55
Gragolha <luka.100.nerozi@gmail.com>
2022-02-02 21:26:11 -0300

    mudanças

---
+++ Lukas/FUNDAMENTOS/Exercicios/fundamentos/teste.js
@@ -0,0 +1,13 @@
+const a = { name: "teste" };
+
+console.log(a);
+
+const b = a;
+
+b.name = "Opa";
+
+console.log(a);
+
+let c = 3;
+
+let d = c;
---
+++ Lukas/nullUndefined.js
@@ -0,0 +1,21 @@
+let valor; // não inicializada
+console.log(valor);
+
+valor = null; // ausência de valor
+console.log(valor);
+//console.log(valor.toString()); //erro!
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto.preco = 3.5;
+console.log(produto);
+
+produto.preco = undefined; //evite atribir undefined
+console.log(!!produto.preco);
+//delete produto.preco
+
+produto.preco = null; //sem preço
+console.log(!!produto.preco);
+console.log(produto);
