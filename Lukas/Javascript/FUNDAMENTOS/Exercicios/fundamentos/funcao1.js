83d4234140d7744c7bd8441febd04cc6b8535b9d
Gragolha <luka.100.nerozi@gmail.com>
2022-02-09 17:34:35 -0300

    add

--- Lukas/Javascript/FUNDAMENTOS/Exercicios/fundamentos/funcao1.js
+++ Lukas/Javascript/FUNDAMENTOS/Exercicios/fundamentos/funcao1.js
@@ -4,3 +4,11 @@ function imprimirSoma(a, b) {
 }
 
 imprimirSoma(2, 3);
+
+//Função com retorno
+function retorno(a, b = 2) {
+  return a + b;
+}
+
+console.log(retorno(2, 3));
+console.log(retorno());
