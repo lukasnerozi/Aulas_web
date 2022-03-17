5897a93716591cc3a3c1b4797ae0bd58b6c5461b
Gragolha <luka.100.nerozi@gmail.com>
2022-03-17 19:54:38 -0300

    repo

---
+++ Lukas/Javascript/FUNDAMENTOS/Exercicios/fundamentos/relacionais.js
@@ -0,0 +1,23 @@
+console.log("01", "1" == 1);
+console.log("02", "1" === 1);
+console.log("03", "1" != 1);
+console.log("04", "1" !== 1);
+
+console.log("05", 3 < 2);
+console.log("06", 3 > 2);
+console.log("07", 3 <= 2);
+console.log("08", 3 >= 2);
+
+const d1 = new Date(0);
+const d2 = new Date(0);
+console.log("09)", d1 === d2);
+
+/* (==) conteudo é igual 
+(===) se é completamento igual inclui o TIPO
+(!=) inverso do igual (true para false)
+(!==) inverso do igual (true para false)
+
+
+
+
+*/
