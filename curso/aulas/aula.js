3792c1d626c1f24628494200e92be788e97c0029
Gragolha <luka.100.nerozi@gmail.com>
2022-10-26 18:01:45 -0300

    HTML listas e tags

---
+++ curso/aulas/aula.js
@@ -0,0 +1,9 @@
+let hora = 21;
+
+if (hora > 6 && hora < 12) {
+  console.log("bom dia");
+} else if (hora > 12 && hora < 18) {
+  console.log("boa tarde");
+} else {
+  console.log("boa noite");
+}
